669f081175ab48c435020bd55f032b1d7c23f687
preek <alain.lafon@dispatched.ch>
2010-12-04 21:29:25 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,36 @@
+ABOUT
+-----
+
+This Python script parses a [Things](http://culturedcode.com/things/) database
+and writes an outline.
+
+Usage
+-----
+
+    $python read_things.py 
+    Usage: read_things.py [options]
+
+    Options:
+      -h, --help            show this help message and exit
+      -d DB, --database=DB  path to things Database.xml
+      -o OTL, --output=OTL  Output outline
+
+
+Sample Output
+-------------
+
+    Review
+            Gather loose papers
+            Process notes
+            Review Someday
+            Review previous calendar data
+            Review waiting for
+            Review "Action" re Mail
+            Review upcoming calendar data
+            Review "Deferred" re Mail
+            Review "Waiting" re Mail
+            Review project lists
+            Review next action list
+            Empty head
+            Review "Incubation" re Mail
+            Review "Respond" re Mail
---
+++ ReadThingsDB.py
@@ -0,0 +1,76 @@
+# -*- coding: utf-8 -*-
+
+from xml.dom import minidom
+import sys
+from optparse import OptionParser
+
+def readThingsDB(things_xml_path, outline_file):
+  xml = minidom.parse(things_xml_path)
+  objects = xml.getElementsByTagName("object")
+  
+  # Get projects
+  projects = []
+  for object in objects:
+    if object.attributes["type"].value != 'TODO':
+      continue
+    attribute_nodes = object.getElementsByTagName("attribute")
+    title = ""
+    for attribute_node in attribute_nodes:
+      # Get all project names
+      # <attribute name="title" type="string">Movies</attribute>
+      if attribute_node.attributes['name'].value == 'title':
+        title = attribute_node.childNodes[0].nodeValue.encode("utf-8")
+        
+        # Get ref_ids of all todos for current project
+        relationship_nodes = object.getElementsByTagName("relationship")
+        ref_ids = ''
+        for relationship_node in relationship_nodes:
+          try:
+            # <relationship name="children" idrefs="z4383 z2529 z6641...">
+            if relationship_node.attributes['name'].value == 'children' and\
+                relationship_node.attributes['idrefs'].value != '':
+              ref_ids = relationship_node.attributes['idrefs'].value
+              break
+          except:
+            # Not all projects have todos -> hence the lookup for 'idrefs' can fail
+            pass
+        
+        # Todos and Projects both are objects with attributes 'todo'. They differ
+        #in that projects have multile ref_ids.
+        if len(ref_ids.split()) > 1:
+          projects.append({'title': title, 'ref_ids': ref_ids, 'todos':[]})
+  
+  # Get todos for each project
+  for project in projects:
+    for ref_id in project['ref_ids'].split():
+      for object in objects:
+        if object.attributes['id'].value == ref_id:
+          attribute_nodes = object.getElementsByTagName("attribute")
+          title = ""
+          for attribute_node in attribute_nodes:
+            if attribute_node.attributes['name'].value == 'title':
+              title = attribute_node.childNodes[0].nodeValue.encode("utf-8")
+              break
+          project['todos'].append(title)
+ 
+  fp = open(outline_file, "w")
+  for project in projects:
+    fp.write(project['title'] + "\n")
+    for todo in project['todos']:
+      fp.write("\t%s"%todo + "\n")
+    print "\n"
+  fp.close()
+
+if __name__ == "__main__":
+  parser = OptionParser()
+  parser.add_option("-d", "--database", 
+                    help="path to things Database.xml", metavar="DB")
+  parser.add_option("-o", "--output", 
+                    help="Output outline", metavar="OTL")
+  
+  (options, args) = parser.parse_args()
+  if not options.database or not options.output:
+    parser.print_help()
+    sys.exit(1)
+  else:
+    readThingsDB(options.database, options.output)
